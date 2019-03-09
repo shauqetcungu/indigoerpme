4b5d6534589afa198de7820083d9be98aa81f28a
shauqetcungu <shauqetcungu1991@gmail.com>
2019-03-09 13:16:35 +0100

    Initial Commit

---
+++ Indigo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Indigo
+//
+//  Created by Saša Vujanovic on 09/03/2019.
+//  Copyright © 2019 Shauqet Cungu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
